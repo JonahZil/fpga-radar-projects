b5a1a07facfcac591df00fefeef6671fad575afa
Jonah Zilberter <onerov810@gmail.com>
2026-03-04 20:16:37 +0100

    Added the delay line module

---
+++ 32-point-sdf/delay_line.v
@@ -0,0 +1,41 @@
+module delay_line # (
+    parameter length = 4
+) (
+    input clk,
+    input rst,
+    
+    input advance_valid,
+    input signed [15:0] data,
+    
+    output reg signed [15:0] out,
+    output reg out_valid
+);
+
+    localparam ADDR_WIDTH = (length > 1) ? $clog2(length) : 0;
+    
+    reg [ADDR_WIDTH - 1:0] pointer;
+    reg signed [15:0] mem [0:length - 1];
+    
+    always @ (posedge clk or posedge rst) begin
+        if(rst) begin
+            pointer <= 0;
+            out <= 0;
+            out_valid <= 0;
+        end else begin
+            if(advance_valid) begin
+                out_valid <= 1;
+                //Output old data at old pointer
+                out <= mem[pointer];
+                
+                //Input new data at old pointer
+                mem[pointer] <= data;
+                
+                //Increment pointer  
+                pointer <= pointer + 1;
+            end else begin
+                out_valid <= 0;
+            end
+        end
+    end
+
+endmodule
